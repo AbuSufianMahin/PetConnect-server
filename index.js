ebda68d02ee582949f0bc07ed9ca41ae5a2d7a34
Abu Sufian Mahin <abusufianmahin3@gmail.com>
2025-07-14 01:05:49 +0600

    added api to add userInfo into db

--- index.js
+++ index.js
@@ -1,11 +1,70 @@
-const express = require('express')
-const app = express()
-const port = process.env.POST || 3000;
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
-  res.send('Hosting PetConnect server')
+  res.send('Hello World!')
 })
 
+const uri = `mongodb+srv://${process.env.PETCONNET_USER}:${process.env.PETCONNET_PASSWORD}@cluster0.udgfocl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    await client.connect();
+
+    const db = client.db("PetConnect");
+
+    const usersCollection = db.collection("users");
+
+    app.post("/users", async (req, res) => {
+      const userData = req.body;
+
+      try {
+        const existingUser = await usersCollection.findOne({ email: userData.email });
+        if (existingUser) {
+          return res.send({ message: "User with this email already exists" });
+          // 
+          // return;
+        }
+
+        // If not exists, insert the new user
+        const result = await usersCollection.insertOne(userData);
+
+        res.status(201).json({
+          message: "User saved successfully",
+          insertedId: result.insertedId,
+        });
+      } catch (err) {
+        res.status(500).json({ message: "Database error", error: err.message });
+      }
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  }
+  finally { }
+}
+run().catch(console.dir);
+
+
 app.listen(port, () => {
-  console.log(`PetConnect is listening on port ${port}`)
+  console.log(`PetConnect is listening to port ${port}`)
 })
